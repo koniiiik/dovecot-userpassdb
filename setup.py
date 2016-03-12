80e09aaaf5e1744dc17dd05dbd2e28bda3406f05
Michal Petrucha <michal.petrucha@koniiiik.org>
2016-03-12 22:05:38 +0100

    Post-release version bump.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='dovecot-userpassdb',
-    version='0.1.0',
+    version='0.1.1.dev0',
     description='Dovecot user-controllable passwords',
     long_description=long_description,
 
